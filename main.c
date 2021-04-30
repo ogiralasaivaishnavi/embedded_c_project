9630cc7679071653d907e352afddbc1c7cb4ea1a
ogiralasaivaishnavi <ogiralavaishu@gmail.com>
2021-04-30 09:21:36 +0530

    activity2

--- main.c
+++ main.c
@@ -1,28 +1,61 @@
-/*
+/**
+ * @file main.c
+ * @author ogiralasaivaishnavi
+ * @brief 
+ * @version 0.1
+ * @date 2021-04-24
+ * 
+ * @copyright Copyright (c) 2021
+ * 
  */
+
 
 #include <avr/io.h>
 
 int main(void)
 {
-    DDRB |=(1<<PB0);  // set B0=1
-    DDRD&=~(1<<PD0);
-    PORTD| =(1<<PD0);
-    DDRD&=~(1<<PD4);
-    PORTD|=(1<<PD4);
+    DDRB |=(1<<PB0); /**
+     * @brief B0 is set as output
+     * 
+     */
+    DDRD&=~(1<<PD0);/**
+     * @brief  D0 is set as Input
+     * 
+     */
+    PORTD| =(1<<PD0);/**
+     * @brief Enable PULL UP for input port
+     * 
+     */
+    DDRD&=~(1<<PD4);/**
+     * @brief  D4 is set as Input
+     * 
+     */
+    PORTD|=(1<<PD4);/**
+     * @brief Enable PULL UP for input port
+     * 
+     */
 
     while(1){
         if(!(PIND&(1<<PD0)))
         {
-           if(!(PIND&(1<<PD4))){
+           if(!(PIND&(1<<PD4)))/**
+                 * @brief SEATED AND SWITCHED ON THE HEATER
+                 * 
+                 */{
                   PORTB| = (1<<PB0);
            }
-           else{
+           else{ /**
+                 * @brief SEATED BUT DIDN'T SWITCH ON THE HEATER
+                 * 
+                 */
             PORTB&=~(1<<PB0);
            }
 
         }
-          else{
+          else{/**
+             * @brief NOT SEATED
+             * 
+             */
                    PORTB&=~(1<<PB0);
           }
     }
